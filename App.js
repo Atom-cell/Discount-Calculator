b02d3e28cefd4c132cd1a3f51e4a123d1b1b0cf6
Atom-cell <rajpootsaniusnain@gmail.com>
2021-11-03 12:39:30 +0500

    task2

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
+  const [price, setPrice] = React.useState(0);
+  const [discount, setDiscount] = React.useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 30 }}>Discount App</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="ENTER PRICE"
+        value={price}
+        onChangeText={(price) => setPrice(price)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="ENTER DISCOUNT %"
+        value={discount}
+        onChangeText={(discount) => setDiscount(discount)}
+      />
     </View>
   );
 }
@@ -17,5 +30,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    padding: 20,
+  },
+  input: {
+    padding: 20,
+    backgroundColor: "#cfcfcf",
+    marginVertical: 20,
+    width: "100%",
+    borderRadius: 50,
+    fontSize: 20,
   },
 });
